3990821684452ac42453f8439c2b7d6a0a26df7c
hot-day <476992585@qq.com>
2019-07-16 01:27:40 -0700

    ----

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	print("----------------");
+	print("----------------");
+	print("----------------");
+	print("----------------");
+	return 0;
+}
